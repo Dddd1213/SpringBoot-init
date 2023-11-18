fe4b8adf715152feec6e3d0ef83f5293c83f33d4
xiaoyangii_ <xiaoyangii@qq.com>
2023-11-18 20:55:11 +0800

    1

--- src/main/java/com/yangmao/config/WebMvcConfiguration.java
+++ src/main/java/com/yangmao/config/WebMvcConfiguration.java
@@ -30,6 +30,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      *
      * @param registry
      */
+    @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册登录拦截器...");
         registry.addInterceptor(loginInterceptor)
@@ -78,6 +79,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      * 设置静态资源映射
      * @param registry
      */
+    @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
